7ebb657247a3ac9d01d9c5dd868e3d0e13989a95
Fírnen (jake moisan) <jacob.moisan@maine.edu>
2018-01-30 15:05:54 -0500

    Say Hi to alice

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World");
 		System.out.println("Goodbye");
+		System.out.println("Hello Alice");
 	}
 
 }
